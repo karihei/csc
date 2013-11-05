74b069901fc721a943aa14e19ed22046089f3509
karihei <kariheeeei@gmail.com>
2013-11-05 21:43:27 +0900

    add dev api

---
+++ api/controllers/DevController.js
@@ -0,0 +1,6 @@
+module.exports = {
+    getuser: function (req, res) {
+        res.json({name: 'user1'});
+    },
+    _config: {}
+  };
--- assets/linker/js/client.js
+++ assets/linker/js/client.js
@@ -1,12 +1,10 @@
-SERVER_URI = 'http://' + document.domain + ':8080';
+SERVER_URI = 'http://' + document.domain + ':1337';
 socket = io.connect(SERVER_URI);
 
 $(document).ready(function() {
-    socket.emit('getuser');
-
-    socket.on('getuser result', function(result) {
+    socket.get('/dev/getuser', {}, function(user) {
         var statusEjs = new EJS({url: '/linker/js/ejs/status.ejs'});
-        var statusHtml = statusEjs.render({name: 'user1'});
+        var statusHtml = statusEjs.render({name: user.name});
         var rContainer = $('#right-container');
         rContainer.append($(statusHtml));
     });
