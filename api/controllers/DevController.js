64b75bb6a19d1bc2b6dc59a1f7f7358ff2308c05
karihei <kariheeeei@gmail.com>
2013-11-06 08:28:32 +0900

    add config/users.js

--- api/controllers/DevController.js
+++ api/controllers/DevController.js
@@ -1,5 +1,6 @@
 module.exports = {
     getuser: function (req, res) {
+        // dummy
         res.json({name: 'user1'});
     },
     _config: {}
---
+++ config/users.js
@@ -0,0 +1,13 @@
+// 参加者一覧のdomainと表示名の紐付けなどを定義する
+// この設定は sails.config['users'] でアクセス可能
+module.exports = {
+    users: [
+        {domain: 'hagihara.com'},
+        {domain: 'karix.jp'},
+        {domain: 'hagix.jp'},
+        {domain: 'hagihara.com'},
+        {domain: 'karikawa.com'},
+        {domain: 'csc.jp'},
+        {domain: 'hoge.com'}
+    ]
+};
